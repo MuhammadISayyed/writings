45f5109b075e1939f01dcfa81d00b12598c35c09
Muhammad I. Sayyed <muhammadisayyed@gmail.com>
2023-12-10 18:43:19 +0200

    Remove the base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://muhammadisayyed.github.io',
-  base: '/writings',
+  // base: '/writings',
   integrations: [mdx()],
 })
